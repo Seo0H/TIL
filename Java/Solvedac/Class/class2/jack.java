151d1cd2e7d98694e6f6301f4ad678d5939565cd
SEO <108770949+Seo0H@users.noreply.github.com>
2022-11-21 05:47:05 +0900

    11/21 TIL

--- Java/Solvedac/Class/class2/jack.java
+++ Java/Solvedac/Class/class2/jack.java
@@ -1,8 +1,23 @@
 package Solvedac.Class.class2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class jack {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] firstInput = br.readLine().split(" ");
+		String[] secondInput = br.readLine().split(" ");
+		int N = Integer.parseInt(firstInput[0]); //카드 개수
+		int M =Integer.parseInt(firstInput[1]); // 합의 근사치 기준
+		
+		int[] arrN = new int[N];
+		for(int i=0; i<N; i++)
+			arrN[i] = Integer.parseInt(secondInput[i]);
+		
 		
 	}
 
