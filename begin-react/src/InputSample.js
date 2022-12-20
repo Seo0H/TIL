1e3dc3330eaa8232698b0d28e12270a40e91e93f
seo <emma05127@gmail.com>
2022-12-20 17:48:58 +0900

    1220 TIL React

---
+++ begin-react/src/InputSample.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function InputSample() {
+    const [inputs, setInputs ] = useState({
+        name: '',
+        nickname: '',
+    });
+
+    const {name, nickname} = inputs; //비구조화 할당을 통한 값 추출
+
+    const onChange = (e) => {
+        const { value, name } = e.target; //우선 e.target 에서 name 과 value 를 추출
+        setInputs({
+            ...inputs, //기존 inputs 객체 복사
+            [name]: value // name 키를 가진 값을 value로 설정
+        });
+    };
+
+    const onReset = () => {
+        setInputs({
+            name: '',
+            nickname: '',
+        })
+    };
+
+    return (
+        <div>
+            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
+            <button onClick={onReset}> 초기화 </button>
+            <div>
+                <b>값: </b>
+                {name} ({nickname})
+            </div>
+        </div>
+    );
+}
+
+export default InputSample;
