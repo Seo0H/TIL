5c39ab2d00ca8a071cb66b766fa5568de8dc972a
SEO <108770949+Seo0H@users.noreply.github.com>
2022-12-22 01:30:23 +0900

    1222 TIL

--- React/react-egoing-coding/src/App.js
+++ React/react-egoing-coding/src/App.js
@@ -1,68 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {Link} from 'react';
+import React, { Link } from 'react';
 
-function Header(props){
-  return(
+function Header(props) {
+  return (
     <header>
-        <h1><a href="/">{ props.title }</a></h1>
+      <h1>
+        <a href="/" onClick={e => {
+          e.preventDefault();
+          props.onChangeMode();
+        }}>{props.title}</a>
+      </h1>
     </header>
   );
 }
 
-function Nav(props){
-  // const {id, title, body} = props.topics;
-  const lis = [
-    <li><a href="/read/1">html</a></li>,
-    <li><a href="/read/2">css</a></li>,
-    <li><a href="/read/3">js</a></li>,
-  ];
+function Nav(props) {
+  const lis = [];
 
-  return(
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+    <li key={t.id}>
+      <a id={t.id}
+        href={`/read/${t.id}`}
+        onClick={e => {
+        e.preventDefault();
+        props.onChangeMode(e.target.id);
+      }}>
+      {t.title}
+      </a>
+    </li>
+    );
+  }
+
+  // 자동으로 생성한 테그에는 리액트가 추적을 할 때 근거가 필요하다.
+  // 따라서 고유한 키 값을 넣어줘야 한다.
+
+  return (
     <nav>
-        <ol>
-          {props.map(id => (
-            <li><Link to={`/read/${id}`}>{title}</Link></li>
-          ))}
-        </ol>
-      </nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
   );
 }
 
-function Article(props){
-  return(
+function Article(props) {
+  return (
     <article>
-        <h2>{props.title}</h2>
-        {props.body}
+      <h2>{props.title}</h2>
+      {props.body}
     </article>
   );
 }
 
-function App() {
 
+function App() {
   const topics = [
-  {
-    id:1,
-    title:'html',
-    body:'html is ...'
-  },
-  {
-    id:2,
-    title:'css',
-    body:'css is ...'
-  },
-  {
-    id:3,
-    title:'js',
-    body:'js is ...'
-  },
-];
+    {
+      id: 1,
+      title: 'html',
+      body: 'html is ...'
+    },
+    {
+      id: 2,
+      title: 'css',
+      body: 'css is ...'
+    },
+    {
+      id: 3,
+      title: 'js',
+      body: 'js is ...'
+    },
+  ];
 
   return (
     <div>
-      <Header title="REACT"/>
-      <Nav topics = {topics} />
-      <Article title="Welcome" body="Hello, WEB"/>
+      <Header title="REACT" onChangeMode={() => { alert('Header'); }} />
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }} />
+      <Article title="Welcome" body="Hello, WEB" />
     </div>
   );
 }
